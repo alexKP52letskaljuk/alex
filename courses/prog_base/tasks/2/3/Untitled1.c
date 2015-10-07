9957018e27e4afc04456183a8ba69f3b694a661e
alexKP52letskaljuk <letskaljuko@gmail.com>
2015-10-07 17:53:42 +0300

    3

---
+++ courses/prog_base/tasks/2/3/Untitled1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+ int a = 10;
+ int i = 3;
+ switch(i){
+case 0:
+    printf("nol");
+    break;
+case 1:
+    printf("odin")
+    break;
+case 2 ... 9:
+    prinf("decimal");
+    break;
+case 77:
+case 777:
+case 7777:
+    prinf("bingo");
+    break;
+default:
+    printf("error");
+}
+    return 0;
+}
--- courses/prog_base/tasks/hello world/hello.c
+++ courses/prog_base/tasks/hello world/hello.c
@@ -1,9 +1,54 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <stdlib.h>
 int main()
 {
-    int a=1, b=3;
-    //hello world
-  printf("Hello, world!\n");
-  return 0;
+    int h, m, code, price, a;
+    cout<<"Vedit kilkist hodun ta xvulun: ";
+    cin>>h>>m;
+    price=(60*h)+m
+    cout<<"Vedite nomer goroda: ";
+    cin>>code;
+    if(code>=10&&code<=999)
+
+
+    switch(code){
+case 44:
+    cout<<"Kyiv";
+    cout<<"Price = 0.77 hrn/xv"
+    a=price*0.77
+    cout<<"Zena zvonka : "<<a;
+    break;
+case 62:
+    cout<<"Donetsk";
+    cout<<"Price = 0.8 hrn/xv"
+    a=price*0.8
+    cout<<"Zena zvonka : "<<a;
+    break;
+case 32:
+    cout<<"Lviv";
+    cout<<"Price = 0.95 hrn/xv"
+    a=price*0.95
+    cout<<"Zena zvonka : "<<a;
+    break;
+case 692:
+    cout<<"Sevastopol";
+    cout<<"Price = 1.5 hrn/xv"
+    a=price*1.5
+    cout<<"Zena zvonka : "<<a;
+    break;
+case 697:
+    cout<<"Simferopol";
+    cout<<"Price = 1.5 hrn/xv"
+    a=price*1.5
+    cout<<"Zena zvonka : "<<a;
+    break;
+
+default:
+    cout<<"error";
+    }
+    else
+        cout<<"error";
+    return 0;
+
 }
