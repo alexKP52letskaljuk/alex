ad95c33c31f3cd3452e9d7ffbf834409acf0f514
alexKP52letskaljuk <letskaljuko@gmail.com>
2015-11-29 23:18:54 +0200

    sf

---
+++ courses/prog_base/tasks/struct/main.c
@@ -0,0 +1,62 @@
+
+//1.Плейліст музики. Кількість плейлістів у яких менше 5 композицій. Перейменувати плейліст.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct PlayList{
+char name [20];
+int  a;//chuslo kompozuzii
+};
+int count(int size, struct PlayList arr[size]);
+void change(struct PlayList * pVar, const char * newValue);
+
+int main(){
+struct PlayList b[20];
+int i;
+for(i=0;i<4;i++){
+fflush(stdin);
+printf("enter name\n");
+gets(b[i].name);
+printf("enter number\n");
+scanf("%i", &b[i].a);
+}
+fflush(stdin);
+int val;
+char newName[20];
+printf("Vedit nomer playlisty: ");
+scanf("%i", &val);
+fflush(stdin);
+gets(newName);
+change(&(b[val]), newName);
+printf("Vasha Kilkist playlistiv: %i\n", count(4, b));
+print(&b[val]);
+return 0;
+}
+
+
+
+int count(int size, struct PlayList arr[size])
+{
+int i;
+int p=0;
+for(i=0;i<size;i++)
+{
+    if(arr[i].a >= 5)
+    p++;
+}
+return p;
+}
+void change(struct PlayList * pVar, const char * newValue)
+
+{
+    strcpy(pVar->name, newValue);
+
+}
+void print(struct PlayList * pVar)
+
+{
+
+    puts(pVar->name);
+    printf("Kilkist pisen: %i", pVar->a);
+}
+
