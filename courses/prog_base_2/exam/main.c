c5020feba2f492a267352ad3746978245a6a509b
alexKP52letskaljuk <letskaljuko@gmail.com>
2016-06-13 13:11:18 +0300

    31

--- courses/prog_base_2/exam/main.c
+++ courses/prog_base_2/exam/main.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+int check (char* str)
+{
+ int opened = 0;
+ int closed = 0;
+
+int len = strlen (str);
+
+for (int i = 0; i < len; i++)
+ {
+ if ( str[i] == '(' )
+ opened++;
+ else if ( str[i] == ')' )
+ closed++;
+ else if ( str[i] != ' ' && str[i] != '\t' )
+ return 0;
+
+ if (closed > opened)
+ return 0;
+ }
+ if (closed == opened)
+ return 1;
+ else
+ return 0;
+}
 
 int main()
 {
-int n=100;
-char str[100] = "asfafaf";
-puts(str);
- return 0;
+ char* str1 = " ( ( ( ( ) ) ) )";
+ char* str2 = " ( ( ) ( ) ))";
+ printf ("check1: %d\n", check (str1));
+ printf ("check2: %d\n", check (str2));
 }
-bool check(int n=100, char str[n])
-{
-    int i;
-    for(i=0;i<n;i++){
-        if(str[i]!='('|| str[i]!=')'||str[i]!=' ')
-            return 0;
-    }
-    for(i=0;i<n;i++)
-    {
-        if()
-    }
-
-}
-
