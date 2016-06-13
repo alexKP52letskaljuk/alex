cbb475272d1121434441de003e98f253ae8e625f
alexKP52letskaljuk <letskaljuko@gmail.com>
2016-06-13 12:13:30 +0300

    21

---
+++ courses/prog_base_2/exam/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n=100;
+char str[100] = "asfafaf";
+puts(str);
+ return 0;
+}
+bool check(int n=100, char str[n])
+{
+    int i;
+    for(i=0;i<n;i++){
+        if(str[i]!='('|| str[i]!=')'||str[i]!=' ')
+            return 0;
+    }
+    for(i=0;i<n;i++)
+    {
+        if()
+    }
+
+}
+
